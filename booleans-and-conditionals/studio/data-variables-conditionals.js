131072ece2e4fedbada2412710c3668ac9453db1
YumanGong <gongymsilvia@gmail.com>
2023-09-14 20:26:19 -0500

    update code

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -18,43 +18,43 @@ let preparedForLiftOff = true;
 
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-// if (astronautCount <= 7 && astronautStatus === "ready" && totalMassKg < maximumMassLimit && (fuelTempCelsius >= -300 || fuelTempCelsius <= -150) && fuelLevel === "100%" && weatherStatus === "clear") {
-if (astronautCount <= 7) {
+if (astronautCount <= 7 && astronautStatus === "ready" && totalMassKg < maximumMassLimit && (fuelTempCelsius >= -300 || fuelTempCelsius <= -150) && fuelLevel === "100%" && weatherStatus === "clear") {
+  // if (astronautCount <= 7) {
 
-  // add logic below to verify all astronauts are ready
-  if (astronautStatus === "ready") {
+  //   // add logic below to verify all astronauts are ready
+  //   if (astronautStatus === "ready") {
 
-    // add logic below to verify the total mass does not exceed the maximum limit of 850000
-    if (totalMassKg < maximumMassLimit) {
+  //     // add logic below to verify the total mass does not exceed the maximum limit of 850000
+  //     if (totalMassKg < maximumMassLimit) {
 
-      // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-      if (fuelTempCelsius >= -300 || fuelTempCelsius <= -150) {
+  //       // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+  //       if (fuelTempCelsius >= -300 || fuelTempCelsius <= -150) {
 
-        // add logic below to verify the fuel level is at 100%
-        if (fuelLevel === "100%") {
+  //         // add logic below to verify the fuel level is at 100%
+  //         if (fuelLevel === "100%") {
 
-          // add logic below to verify the weather status is clear
-          if (weatherStatus === "clear") {
+  //           // add logic below to verify the weather status is clear
+  //           if (weatherStatus === "clear") {
 
-            // Verify shuttle launch can proceed based on above conditions
-            console.log("ALL systems are a go! Initiating space shuttle launch sequence.");
-            console.log("----------------------------------------------------------------");
-            console.log("Date:" + date);
-            console.log("Time:" + time);
-            console.log("Astronaut Count:" + astronautCount);
-            console.log("Crew Mass:" + crewMassKg + "kg");
-            console.log("Fuel Mass:" + fuelMassKg + "kg");
-            console.log("Shuttle Mass:" + shuttleMassKg + "kg");
-            console.log("Total Mass:" + totalMassKg + "kg");
-            console.log("Fuel Temprature:" + fuelTempCelsius + "℃");
-            console.log("Weather Status:" + weatherStatus);
-            console.log("----------------------------------------------------------------");
-            console.log("Have a safe trip astronauts!");
-          }
-        }
-      }
-    }
-  }
+  //             // Verify shuttle launch can proceed based on above conditions
+  console.log("ALL systems are a go! Initiating space shuttle launch sequence.");
+  console.log("----------------------------------------------------------------");
+  console.log("Date:" + date);
+  console.log("Time:" + time);
+  console.log("Astronaut Count:" + astronautCount);
+  console.log("Crew Mass:" + crewMassKg + "kg");
+  console.log("Fuel Mass:" + fuelMassKg + "kg");
+  console.log("Shuttle Mass:" + shuttleMassKg + "kg");
+  console.log("Total Mass:" + totalMassKg + "kg");
+  console.log("Fuel Temprature:" + fuelTempCelsius + "℃");
+  console.log("Weather Status:" + weatherStatus);
+  console.log("----------------------------------------------------------------");
+  console.log("Have a safe trip astronauts!");
 } else {
+  //         }
+  //       }
+  //     }
+  //   }
+  // } else {
   console.log("Failed to send");
 }
