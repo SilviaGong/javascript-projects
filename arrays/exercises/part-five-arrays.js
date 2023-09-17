92dd4a2c708164618752170140ebfc060bbc0736
YumanGong <gongymsilvia@gmail.com>
2023-09-17 11:54:13 -0500

    update

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -8,4 +8,30 @@ let arr = ['B', 'n', 'n', 5];
 //3) Do split or join change the original string/array?
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
+
+
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+let charles = ['coder', 'Tech', 47, 23, 350];
+charles.sort();
+console.log(charles);
+
+let classes = ["science, computer, art"];
+let teachers = ["Jones", "Willoughby", "Rhodes"];
+console.log(classes.length);
+console.log(teachers.length);
+
+let string1 = 'LaunchCode students delimiter rock!';
+console.log(string1.split(" "));
+console.log(string1.split(""));
+console.log(string1.split("delimiter"));
+
+let string2 = ["LaunchCode", "Students", "Rock"];
+console.log(string2.join(" "));
+console.log(string2.join(""));
+
+
+let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
+let selectedItems = [];
+
+selectedItems = groceryBag.slice(2, 5).sort();
+console.log(selectedItems);
--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -1,4 +1,23 @@
 let jsCreator = "Brendan Eich";
+let firstInitial = jsCreator[0];
+let lastInitial = jsCreator[8];
+let phrase = "JavaScript rocks!";
+
+console.log(phrase.length);
+console.log(phrase[phrase.length - 8]);
+
 
 console.log(jsCreator[-1]);
-console.log(jsCreator[42]);
+console.log(jsCreator[42] === '');
+
+let outputStr = "JavaScript was created by somebody with initials " +
+  firstInitial + "." +
+  lastInitial + ".";
+
+console.log(outputStr);
+
+let cityName = "Vienna";
+let stateName = "Virginia";
+location = cityName + ", " + stateName;
+
+console.log(location.length);
