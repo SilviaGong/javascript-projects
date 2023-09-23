208cf5e59ad5e5a5926b8a325e37cbd0a3c8d1fa
YumanGong <gongymsilvia@gmail.com>
2023-09-23 12:58:26 -0500

    update

--- functions/try-it/reverse.js
+++ functions/try-it/reverse.js
@@ -1,5 +1,24 @@
 function reverse(str) {
    let lettersArray = str.split('');
    let reversedLettersArray = lettersArray.reverse();
-   return reversedLettersArray.join('');
+   return reversedLettersArray.join(' ');
 }
+
+function reverse(str) {
+   let reversed = '';
+
+   for (let i = 0; i < str.length; i++) {
+      reversed = str[i] + reversed;
+   }
+   return reversed;
+}
+
+
+function reverse(str) {
+   return str.split('').reverse().join('');
+}
+
+function isPalindrome(str) {
+   return reverse(str) === str;
+}
+console.log(reverse("myString"));
--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,49 @@
-function sayHello() {
-   console.log("Hello, World!");
+/* function sayHello(name) {
+   return `Hello, ${name}!`;
 }
+console.log(sayHello("Yuman"));
+
+
+function countToN(n) {
+   let count = 5;
+   while (true) {
+      if (count > n) {
+         return n;
+      }
+      console.log(count);
+      count++;
+   }
+}
+console.log(countToN(2));
+
+
+function plusTwo(num) {
+   return num + 2;
+}
+let a = 2;
+for (let i = 0; i < 4; i++) {
+   a = plusTwo(a);
+}
+console.log(a); */
+
+function repeater(str) {
+   let repeated = str + str;
+   console.log(repeated);
+}
+repeater('Bob');
+
+
+/* let message = "Hello, World!";
+function printMessage() {
+   console.log(message);
+}
+printMessage();
+message = "Goodbye";
+printMessage();
+
+
+let num = 42;
+function isEven(num) {
+   return num % 2 === 0;
+}
+console.log(isEven(43)); */
