6363d91c1eded7b63c57355275b0a45e69b1c3ed
YumanGong <gongymsilvia@gmail.com>
2023-09-29 22:51:41 -0500

    update

--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -1,17 +1,44 @@
-function randomSelection(arr){
-   let index = Math.floor(Math.random()*arr.length);
-   return arr[index];
- }
- 
- let happiness = ['Hope', 'Joy', 'Peace', 'Love', 'Kindness', 'Puppies', 'Kittens', 'Tortoise'];
- 
- let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
- 
- for (i=0; i < 8; i++){
-   console.log(randomSelection(happiness));
- }
- 
- //Experiment with the code above. Try to:
- //a) Print 3 random selections from each array.
- //b) Have the code randomly pick one array, and then print 2 random items from it.
- //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+function randomSelection(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
+let happiness = ['Hope', 'Joy', 'Peace', 'Love', 'Kindness', 'Puppies', 'Kittens', 'Tortoise'];
+
+let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
+
+/* for (i = 0; i < 8; i++) {
+  console.log(randomSelection(happiness));
+} */
+
+//Experiment with the code above. Try to:
+//a) Print 3 random selections from each array.
+//b) Have the code randomly pick one array, and then print 2 random items from it.
+//c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+
+// Printing 3 random selections from happiness array
+for (let i = 0; i < 3; i++) {
+  console.log("Random happiness:" + randomSelection(happiness));
+}
+
+// Printing 3 random selections from words array
+for (let i = 0; i < 3; i++) {
+  console.log("Random words:" + randomSelection(words));
+}
+
+//b) Have the code randomly pick one array, and then print 2 random items from it.
+let randomArray = randomSelection([happiness, words]);
+for (let i = 0; i < 2; i++) {
+  console.log(randomSelection(randomArray));
+}
+
+
+//c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+let newArray = randomSelection([happiness, words]);
+console.log("New item:" + randomSelection(newArray));
+
+
+/* let newArray = [];
+newArray.push(randomSelection(happiness));
+newArray.push(randomSelection(words));
+console.log(newArray); */
--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,15 +2,57 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {
+      return Math.floor(Math.random() * 11);
+   }
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {
+      return Math.round(Math.random() * 10);
+   }
 };
+
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {
+      return Math.round(Math.random() * 10);
+   }
+};
+
+let beagle = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {
+      return Math.round(Math.random() * 10);
+   }
+};
+
+let tardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {
+      return Math.round(Math.random() * 10);
+   }
+};
+
 
 
 // After you have created the other object literals, add the astronautID property to each one.
@@ -20,3 +62,31 @@ let salamander = {
 // Print out the relevant information about each animal.
 
 // Start an animal race!
+
+
+let crew = [superChimpOne, salamander, superChimpTwo, beagle, tardigrade];
+
+function screwReports(crew) {
+   for (let i = 0; i < crew.length; i++) {
+      let crewKey = crew[i];
+      console.log(`${crewKey["name"]} is a ${crewKey["species"]}. They are ${crewKey["age"]} years old and ${crewKey["mass"]} kilograms. Their ID is ${crewKey["astronautID"]}.`);
+   }
+}
+
+screwReports(crew);
+
+
+function fitnessTest(candidates) {
+   let turns = 0;
+   let movesteps = 0;
+   let results = [];
+   for (let i = 0; i < candidates.length; i++) {
+      while (movesteps < 20) {
+         movesteps += candidates[i].move();
+         turns++;
+      }
+      results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+}
+console.log(fitnessTest(crew));
