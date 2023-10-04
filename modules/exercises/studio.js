0a93efb20ce70a50d167621569247f1ce3d67b50
YumanGong <gongymsilvia@gmail.com>
2023-10-03 22:37:04 -0500

    update

---
+++ modules/exercises/studio.js
@@ -0,0 +1,70 @@
+
+//1.Use code to print “Hello, World” to the screen.
+console.log("Hello, World");
+
+//2.Define and initialize variables
+let name = "John";
+let age = 30;
+
+// 3.Change variables
+name = "Alice";
+age = 25;
+
+// 4.Convert the string '1234' into a number
+const strNumber = '1234';
+const number = Number(strNumber);
+
+
+//5.Construct a for loop to repeat a task 100 times
+for (let i = 0; i < 100; i++) {
+  console.log("This is iteration number: " + i);
+}
+
+// 6.Construct if/else if/else statements to decide which task to perform
+const condition = true;
+
+if (condition) {
+  console.log("Task 1");
+} else if (!condition) {
+  console.log("Task 2");
+} else {
+  console.log("Task 3");
+}
+
+
+// 7.Build, modify, and access an array
+const fruits = ["apple", "banana"];
+fruits.push("orange");
+console.log(fruits[0]);
+
+// 8.Design and call a function
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Alice");
+
+// 9.Call one function from within another function
+function add(a, b) {
+  return a + b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(3, add(2, 4));
+console.log("Result:", result);
+
+// 10.Find and fix bugs in a segment of non-working code
+function calculateSum(arr) {
+  let sum = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    // Bug: Index should be '<' instead of '<='
+    sum += arr[i];
+  }
+  return sum;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log("Sum:", calculateSum(numbers));
+
