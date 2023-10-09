d735ce671edf4cfaef3142c302bca3095edeeed6
YumanGong <gongymsilvia@gmail.com>
2023-10-08 20:23:20 -0500

    update

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,58 @@
 
+/* let launchcode = {
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (num) {
+    if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+      return 'LaunchCode Rocks!';
+    } else if (num % 2 === 0 && num % 3 === 0) {
+      return 'LaunchCode!';
+    } else if (num % 2 === 0 && num % 5 === 0) {
+      return 'Launch Rocks! (CRASH!!!!)';
+    } else if (num % 3 === 0 && num % 5 === 0) {
+      return 'Code Rocks!';
+    } else if (num % 2 === 0 && num % 5 === 0) {
+      return 'Launch Rocks!';
+    } else if (num % 2 === 0) {
+      return 'Launch!';
+    } else if (num % 3 === 0) {
+      return 'Code!';
+    } else if (num % 5 === 0) {
+      return 'Rocks!';
+    } else {
+      return "Rutabagas! That doesn't work.";
+    }
+  }
+} */
+
+
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (num) {
+    const divisors = [2, 3, 5];
+    const messages = ['Launch', 'Code', 'Rocks'];
+
+    let output = '';
+
+    for (let i = 0; i < divisors.length; i++) {
+      if (num % divisors[i] === 0) {
+        output += messages[i];
+      }
+    }
+    if (output === '') {
+      return "Rutabagas! That doesn't work.";
+    } else {
+      return output + '!';
+    }
+  }
 }
 
 module.exports = launchcode;
+
+
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,47 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function () {
 
   // Write your unit tests here!
-  
+  test("check that the value of organization is 'nonprofit'", function () {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("check that the value of executiveDirector is 'Jeff'", function () {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+  test("check that the value of percentageCoolEmployees is 100", function () {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  test("check that programsOffered is correctly set", function () {
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+  test("When passed a number that is ONLY divisible by 2, return 'Launch!'", function () {
+    expect(launchcode.launchOutput(4)).toBe('Launch!');
+  });
+  test("When passed a number that is ONLY divisible by 3, return 'Code!'", function () {
+    expect(launchcode.launchOutput(9)).toBe('Code!');
+  });
+  test("When passed a number that is ONLY divisible by 5, return 'Rocks!'", function () {
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+  });
+  test("When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'", function () {
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+  });
+  test("When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'", function () {
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+  });
+  test("When passed a number that is divisible by 2 AND 5, return 'Launch Rocks! (CRASH!!!!)'", function () {
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+  });
+  test("When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'", function () {
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+  });
+  test("When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'", function () {
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work.");
+  });
 });
