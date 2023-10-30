b9b3671ec28fd2789f9b4268c60a3d4c0dfcfdbf
YumanGong <gongymsilvia@gmail.com>
2023-10-29 19:27:00 -0500

    update

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,24 @@
-function init () {
+function init() {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', function () {
+        paragraph.innerHTML = 'Houston, we have liftoff!';
+    });
+    missionAbort.addEventListener('mouseover', function () {
+        missionAbort.style.background = 'red';
+    });
+    missionAbort.addEventListener('mouseout', function (event) {
+        event.target.style.background = "";
+    });
+    missionAbort.addEventListener('click', function () {
+        let confirm = window.confirm("Are you sure you want to abort the mission?");
+        if (confirm === true) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    });
 }
 
 window.addEventListener("load", init);
